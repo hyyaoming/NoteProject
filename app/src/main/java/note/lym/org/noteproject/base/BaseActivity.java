072ac9994ea45f3ac70c525c80d92acb06812dee
liyaoming <liyaoming@wepiao.com>
2017-04-27 13:08:03 +0800

    添加沉浸式状态栏相关配置

--- app/src/main/java/note/lym/org/noteproject/base/BaseActivity.java
+++ app/src/main/java/note/lym/org/noteproject/base/BaseActivity.java
@@ -6,7 +6,9 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import butterknife.ButterKnife;
+import note.lym.org.noteproject.R;
 import note.lym.org.noteproject.manage.ActivityManage;
+import note.lym.org.noteproject.utils.StatusBarCompat;
 
 /**
  * 基类
@@ -22,6 +24,10 @@ public abstract class BaseActivity extends BaseRunTimePermission {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setContentView(getLayoutId());
+        /**
+         * 这里必须这样配置不然不起效
+         */
+        StatusBarCompat.compat(this, getResources().getColor(R.color.little_gary));
         Log.i(TAG, getClass().getSimpleName());
         ButterKnife.bind(this);
         initData();
@@ -37,7 +43,7 @@ public abstract class BaseActivity extends BaseRunTimePermission {
 
     protected abstract void bindView();
 
-    protected void exitApp(){
+    protected void exitApp() {
         ActivityManage.getInstance().backAllActivityToStack();
         android.os.Process.killProcess(Process.myPid());
     }
