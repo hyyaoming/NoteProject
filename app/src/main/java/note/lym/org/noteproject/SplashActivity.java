9b18729ea0fd23d84ef8a1d53bfb9620af39473b
liyaoming <yaoming.li>
2018-01-22 20:25:02 +0800

    [*]add svg image

--- app/src/main/java/note/lym/org/noteproject/SplashActivity.java
+++ app/src/main/java/note/lym/org/noteproject/SplashActivity.java
@@ -1,26 +1,23 @@
 package note.lym.org.noteproject;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.view.WindowManager;
-import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.opensource.svgaplayer.SVGACallback;
+import com.opensource.svgaplayer.SVGAImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import note.lym.org.noteproject.ui.home.HomePagerActivity;
 
-public class SplashActivity extends AppCompatActivity implements Runnable {
+public class SplashActivity extends AppCompatActivity {
 
-    @BindView(R.id.flb)
-    FloatingActionButton mFloatButton;
-    private static final int POST_DELAYED_TIME = 200;
-    private static final int ANIMATOR_TIME = 1000;
+    @BindView(R.id.iv_svg)
+    SVGAImageView mIvSVG;
+    private static final String TAG = SplashActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +25,28 @@ public class SplashActivity extends AppCompatActivity implements Runnable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
-        mFloatButton.postDelayed(this, POST_DELAYED_TIME);
-    }
-
-    @Override
-    public void run() {
-        final View parentView = (View) mFloatButton.getParent();
-        float scale = (float) (Math.sqrt(parentView.getHeight() * parentView.getHeight() + parentView.getWidth() * parentView.getWidth()) / mFloatButton.getHeight());
-        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", scale);
-        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", scale);
-        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(mFloatButton, scaleX, scaleY).setDuration(ANIMATOR_TIME);
-        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        objectAnimator.addListener(new AnimatorListenerAdapter() {
+        mIvSVG.setCallback(new SVGACallback() {
             @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                mFloatButton.setVisibility(View.GONE);
+            public void onPause() {
+                Log.d(TAG, "svg：onPause");
+            }
+
+            @Override
+            public void onFinished() {
+                Log.d(TAG, "svg：onFinished");
                 HomePagerActivity.action(SplashActivity.this);
             }
+
+            @Override
+            public void onRepeat() {
+                Log.d(TAG, "svg：onRepeat");
+            }
+
+            @Override
+            public void onStep(int frame, double percentage) {
+                Log.d(TAG, "svg：onStep");
+            }
         });
-        objectAnimator.start();
     }
 
 }
